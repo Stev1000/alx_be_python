6aad358ed80145e8f92f48be85d9fb0f402cfc56
Stevo <nsanzestevo1@gmail.com>
2024-09-15 00:01:03 +0200

    Click the + icon next to the file or the + next to "Changes" to stage the file.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define the two numbers
+number1 = 10
+number2 = 5
+
+# Perform the operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Print the results explicitly
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference_result)
+print("Multiplication of", number1, "and", number2, "is", product_result)
